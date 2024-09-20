97fa2604821275f0973aaace4e9a14d053371b3f
Matthewh1225 <mhuckman97@yahoo.com>
2024-09-19 19:37:24 -0500

    idc

--- Testinggg/src/App.java
+++ Testinggg/src/App.java
@@ -2,12 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("mouse"+"cat");
         System.out.println("mouse"+"dog");
-<<<<<<< HEAD
         System.out.println("monkey");
-=======
-        System.out.println("mouse"+"dog"+"potato");
-        
->>>>>>> 9e89e7602e28aaa008342937eff1e9f7fe292a94
     }
     
 }
